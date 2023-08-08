0a50db2f5ba35525cf36033d20676bdce91c8fba
Manuel Carbajal <manicar2093@gmail.com>
2023-08-08 02:00:29 -0600

    feat: change to AstroJS

---
+++ src/data/services.ts
@@ -0,0 +1,27 @@
+export const services = [
+	{
+		title: "Asesoria presencial",
+		icon: "bx bx-test-tube",
+		description: "¡Conoce las condiciones actuales de tu cuerpo mediante un estudio llamado Biotest!"
+	},
+	{
+		title: "Asesoría a distancia",
+		icon: "bx bx-file",
+		description: "¡Qué y cuánto comer de acuerdo a tu objetivo y tus condiciones!"
+	},
+	{
+		title: "Rutina de Entrenamiento",
+		icon: "bx bx-tachometer",
+		description: "¡Manten tu cuerpo fuerte y sano mediante un programa de actividad física!"
+	},
+	{
+		title: "Suplementos",
+		icon: "bx bxs-capsule",
+		description: " ¡Venta de suplementos para la perdida de grasa y/o ganancia muscular de las mejores marcas!"
+	},
+	{
+		title: "Ropa deportiva",
+		icon: "bx bx-closet",
+		description: "¡Lleva nuestro escudo contigo! y obten nuestros productos oficiales del #CharlyTeam"
+	},
+];
