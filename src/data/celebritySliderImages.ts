035d245ddb187da8d3d11cf03a0cbab3d24bbc0a
Manuel Carbajal <manicar2093@gmail.com>
2023-08-08 02:00:29 -0600

    feat: change to AstroJS

---
+++ src/data/celebritySliderImages.ts
@@ -0,0 +1,78 @@
+export const celebritySlidirImages = [
+    {
+        imgSrc: "assets/img/celebrities/celebrity_1.jpg",
+        imgAlt: "",
+        memberInfo: "Reina Isis (Luchadora Profesional)"
+    },
+    {
+        imgSrc: "assets/img/celebrities/celebrity_2.jpg",
+        imgAlt: "",
+        memberInfo: "Tigre Metalico (el Pedro ingente de la lucha libre)"
+    },
+    {
+        imgSrc: "assets/img/celebrities/celebrity_3.jpg",
+        imgAlt: "",
+        memberInfo: "La Vaquerita (Luchadora Profesional del CMLL)"
+    },
+    {
+        imgSrc: "assets/img/celebrities/celebrity_4.jpg",
+        imgAlt: "",
+        memberInfo: "Paul King (Fundador de Barras Praderas)"
+    },
+    {
+        imgSrc: "assets/img/celebrities/celebrity_5.jpg",
+        imgAlt: "",
+        memberInfo: "Leonardo Riaño (Cronista deportivo de Televisa Deportes)"
+    },
+    {
+        imgSrc: "assets/img/celebrities/celebrity_6.jpg",
+        imgAlt: "",
+        memberInfo: "Rey Bucanero (Luchador profesional)"
+    },
+    {
+        imgSrc: "assets/img/celebrities/celebrity_7.jpg",
+        imgAlt: "",
+        memberInfo: "El tirantes (Referí profesional de lucha libre AAA)"
+    },
+    {
+        imgSrc: "assets/img/celebrities/celebrity_8.jpg",
+        imgAlt: "",
+        memberInfo: "Shocker (Luchador Profesional)"
+    },
+    {
+        imgSrc: "assets/img/celebrities/celebrity_9.jpg",
+        imgAlt: "",
+        memberInfo: "Mago Yambo (Creador de mágia)"
+    },
+    {
+        imgSrc: "assets/img/celebrities/celebrity_10.jpg",
+        imgAlt: "",
+        memberInfo: "Romel Pacheco (Clavadista y medallista olímpico)"
+    },
+    {
+        imgSrc: "assets/img/celebrities/celebrity_11.jpg",
+        imgAlt: "",
+        memberInfo: "Dr. Alfredo Chi (Científico de Pfizer)"
+    },
+    {
+        imgSrc: "assets/img/celebrities/celebrity_12.jpg",
+        imgAlt: "",
+        memberInfo: "Carlos Bravo (Periodista grupo Distinción)"
+    },
+    {
+        imgSrc: "assets/img/celebrities/celebrity_13.jpg",
+        imgAlt: "",
+        memberInfo: "Santiago Taboada (Alcalde de la delegación Benito Juárez Ciudad de México)"
+    },
+    {
+        imgSrc: "assets/img/celebrities/celebrity_14.jpg",
+        imgAlt: "",
+        memberInfo: "Alvin Mendoza y Jagger Martínez (Ex Jugadores Profesionales de fútbol y canteranos del club America)"
+    },
+    {
+        imgSrc: "assets/img/celebrities/celebrity_15.jpg",
+        imgAlt: "",
+        memberInfo: "Reinaldo Navia (Ex Jugador profesional del Club America)"
+    },
+    
+]
