12fcdcc3a9a6629efc5d59ce73bd6a9c0abbb514
Manuel Carbajal <manicar2093@gmail.com>
2025-07-16 21:12:22 -0600

    feat: change index page composition

--- src/data/officialProducts.ts
+++ src/data/officialProducts.ts
@@ -1,27 +1,22 @@
 export const officialProducts = [
     {
         imgSrc: "assets/img/official_products/proteina.jpg",
-        imgAlt: "charly_team_proteina_official",
-        memberInfo: "PROTEINA"
+        imgAlt: "charly_team_proteina_official"
     },
     {
         imgSrc: "assets/img/official_products/creatina.jpg",
-        imgAlt: "charly_team_creatina_official",
-        memberInfo: "CREATINA"
+        imgAlt: "charly_team_creatina_official"
     },
     {
         imgSrc: "assets/img/official_products/glutamina.jpg",
-        imgAlt: "charly_team_glutamina_official",
-        memberInfo: "GLUTAMINA"
+        imgAlt: "charly_team_glutamina_official"
     },
     {
         imgSrc: "assets/img/official_products/oxido.jpg",
-        imgAlt: "charly_team_oxido_official",
-        memberInfo: "ÓXIDO NÍTRICO"
+        imgAlt: "charly_team_oxido_official"
     },
     {
         imgSrc: "assets/img/official_products/bcaas.jpg",
-        imgAlt: "charly_team_bcaas_official",
-        memberInfo: "BCAA'S"
+        imgAlt: "charly_team_bcaas_official"
     },
 ]
